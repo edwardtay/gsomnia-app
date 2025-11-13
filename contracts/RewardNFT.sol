946e2ecb773d5f0c82a0f9a0d4da9921d0828029
edwardtay <edwardtay7@gmail.com>
2025-11-13 21:56:00 +0800

    Clean up test files and scripts

--- contracts/RewardNFT.sol
+++ contracts/RewardNFT.sol
@@ -2,57 +2,34 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Permissionless NFT Claim - reads from Somnia Streams
-interface IStreams {
-    function getAllPublisherDataForSchema(bytes32 schemaId, address publisher) external view returns (bytes[][] memory);
-}
-
-contract RewardNFT is ERC721 {
-    IStreams public immutable streamsContract;
-    bytes32 public immutable schemaId;
-    address public immutable publisher;
-    
+contract RewardNFT is ERC721, Ownable {
     uint256 private _tokenIdCounter;
     mapping(address => mapping(uint256 => bool)) public hasClaimed;
+    mapping(uint256 => address) public milestoneWinners;
     
     event RewardClaimed(address indexed winner, uint256 milestone, uint256 tokenId);
+    event MilestoneSet(uint256 milestone, address winner);
     
-    constructor(address _streamsContract, bytes32 _schemaId, address _publisher) 
-        ERC721("gSomnia Milestone Reward", "GSOMNIA") {
-        streamsContract = IStreams(_streamsContract);
-        schemaId = _schemaId;
-        publisher = _publisher;
+    constructor() ERC721("gSomnia Milestone Reward", "GSOMNIA") Ownable(msg.sender) {}
+    
+    function setMilestoneWinner(uint256 milestone, address winner) external onlyOwner {
+        require(milestone % 10 == 0 && milestone > 0, "Invalid milestone");
+        milestoneWinners[milestone] = winner;
+        emit MilestoneSet(milestone, winner);
     }
     
     function claim(uint256 milestone) external {
         require(milestone % 10 == 0 && milestone > 0, "Invalid milestone");
         require(!hasClaimed[msg.sender][milestone], "Already claimed");
-        
-        bytes[][] memory allData = streamsContract.getAllPublisherDataForSchema(schemaId, publisher);
-        require(allData.length >= milestone, "Milestone not reached");
-        
-        bytes[] memory targetMessage = allData[milestone - 1];
-        address sender = abi.decode(targetMessage[2], (address));
-        require(sender == msg.sender, "Not the milestone sender");
+        require(milestoneWinners[milestone] == msg.sender, "Not the milestone winner");
         
         uint256 tokenId = _tokenIdCounter++;
         _safeMint(msg.sender, tokenId);
         hasClaimed[msg.sender][milestone] = true;
         
         emit RewardClaimed(msg.sender, milestone, tokenId);
-    }
-    
-    function canClaim(address user, uint256 milestone) external view returns (bool) {
-        if (milestone % 10 != 0 || milestone == 0) return false;
-        if (hasClaimed[user][milestone]) return false;
-        
-        bytes[][] memory allData = streamsContract.getAllPublisherDataForSchema(schemaId, publisher);
-        if (allData.length < milestone) return false;
-        
-        bytes[] memory targetMessage = allData[milestone - 1];
-        address sender = abi.decode(targetMessage[2], (address));
-        return sender == user;
     }
     
     function _baseURI() internal view override returns (string memory) {
